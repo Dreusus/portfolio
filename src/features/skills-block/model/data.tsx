3647d6f301c87a27796038ac1ef7ba664325ac6f
Dreusus <pitakxdd@gmail.com>
2025-10-14 23:43:37 +0300

    Пайтог

--- src/features/skills-block/model/data.tsx
+++ src/features/skills-block/model/data.tsx
@@ -1,68 +1,71 @@
-import { cn } from '@/shared/utils/utils';
+import {cn} from '@/shared/utils/utils';
 import ReactIcon from './../ui/react.svg';
 import PythonIcon from '../ui/python.svg';
 import NodeJsIcon from './../ui/node.svg';
-import NextJsIcon from './../ui/nextjs.svg';
+import PydanticIcon from '../ui/pydantic.svg';
 import PytestIcon from '../ui/pytest.svg';
 import PlaywrightIcon from '../ui/pw.svg';
 import Docker from './../ui/docker.svg';
-import Strapi from './../ui/strapi.svg';
+import SeleniumIcon from '../ui/selenium.svg';
 
 export const SKILLS = [
-  {
-    title: 'React',
-    icon: (commonClasses: string) => (
-      <ReactIcon className={cn(commonClasses, 'text-primary')} />
-    ),
-    url: 'https://react.dev/',
-  },
-  {
-    title: 'TypeScript',
-    icon: (commonClasses: string) => (
-      <PythonIcon className={cn(commonClasses, 'text-secondary')} />
-    ),
-    url: 'https://www.python.org/',
-  },
-  {
-    title: 'React Query',
-    icon: (commonClasses: string) => (
-      <PytestIcon className={cn(commonClasses, 'text-primary')} />
-    ),
-    url: 'https://docs.pytest.org/en/stable/',
-  },
-  {
-    title: 'Tailwind CSS',
-    icon: (commonClasses: string) => (
-      <PlaywrightIcon className={cn(commonClasses, 'text-secondary')} />
-    ),
-    url: 'https://tailwindcss.com/',
-  },
-  {
-    title: 'Node.js',
-    icon: (commonClasses: string) => (
-      <NodeJsIcon className={cn(commonClasses, 'text-secondary')} />
-    ),
-    url: 'https://nodejs.org/',
-  },
-  {
-    title: 'Next.js',
-    icon: (commonClasses: string) => (
-      <NextJsIcon className={cn(commonClasses, 'text-primary')} />
-    ),
-    url: 'https://nextjs.org/',
-  },
-  {
-    title: 'Docker',
-    icon: (commonClasses: string) => (
-      <Docker className={cn(commonClasses, 'text-secondary')} />
-    ),
-    url: 'https://www.docker.com/',
-  },
-  {
-    title: 'Strapi',
-    icon: (commonClasses: string) => (
-      <Strapi className={cn(commonClasses, 'text-primary')} />
-    ),
-    url: 'https://strapi.io/',
-  },
+
+    {
+        title: 'Python',
+        icon: (commonClasses: string) => (
+            <PythonIcon className={cn(commonClasses, 'text-secondary')}/>
+        ),
+        url: 'https://www.python.org/',
+    },
+    {
+        title: 'Pytest',
+        icon: (commonClasses: string) => (
+            <PytestIcon className={cn(commonClasses, 'text-primary')}/>
+        ),
+        url: 'https://docs.pytest.org/en/stable/',
+    },
+    {
+        title: 'Playwright',
+        icon: (commonClasses: string) => (
+            <PlaywrightIcon className={cn(commonClasses, 'text-secondary')}/>
+        ),
+        url: 'https://playwright.dev/',
+    },
+    {
+        title: 'Selenium',
+        icon: (commonClasses: string) => (
+            <SeleniumIcon className={cn(commonClasses, 'text-primary')}/>
+        ),
+        url: 'https://www.selenium.dev/',
+
+    },
+
+    {
+        title: 'Pydantic',
+        icon: (commonClasses: string) => (
+            <PydanticIcon className={cn(commonClasses, 'text-primary')}/>
+        ),
+        url: 'https://docs.pydantic.dev/latest/',
+    },
+    {
+        title: 'Docker',
+        icon: (commonClasses: string) => (
+            <Docker className={cn(commonClasses, 'text-secondary')}/>
+        ),
+        url: 'https://www.docker.com/',
+    },
+    {
+        title: 'Node.js',
+        icon: (commonClasses: string) => (
+            <NodeJsIcon className={cn(commonClasses, 'text-primary')}/>
+        ),
+        url: 'https://nodejs.org/',
+    },
+    {
+        title: 'React',
+        icon: (commonClasses: string) => (
+            <ReactIcon className={cn(commonClasses, 'text-secondary')}/>
+        ),
+        url: 'https://react.dev/',
+    }
 ];
