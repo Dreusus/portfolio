acfa351d737553fbca5b7ddfd591c77a32b88b63
Dreusus <pitakxdd@gmail.com>
2025-12-21 19:21:55 +0300

    fix: stupid model

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -7,7 +7,7 @@ client = genai.Client(api_key=config_obj.gemini_api_key)
 
 def get_answer_for_gemini(prompt: str):
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemma-3-12b",
         contents=prompt
     )
     return response.text
